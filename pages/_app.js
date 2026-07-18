624932e6283620846ce7bdca06c8982dd4916471
malegal <ma.legal.eg@outlook.com>
2026-07-18 22:09:26 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
 import '../styles/globals.css';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
+  // تحسين إمكانية الوصول: إضافة لغة الصفحة
+  useEffect(() => {
+    document.documentElement.lang = 'ar';
+    document.documentElement.dir = 'rtl';
+  }, []);
+
   return <Component {...pageProps} />;
 }
 
